d7ffa5b42ee16370c46f75dd9d65ffbf99711e85
Ba Nyar Naing <banyarnaing123@gmail.com>
2021-12-03 19:48:32 +0630

    earn second star

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,6 +1,8 @@
 fun main() {
     val inputs = readIntegers("input01.txt").toMutableList()
 
+    //  part 1
+    /*
     var count = 0
     var previous = inputs.removeFirst()
 
@@ -9,5 +11,26 @@ fun main() {
         previous = current
     }
 
+    println(count)
+     */
+
+    //  part 2
+    var count = 0
+    var previous = 0
+
+    while (true) {
+        try {
+            if (previous == 0) {
+                previous = inputs.component1() + inputs.component2() + inputs.component3()
+            } else {
+                val current = inputs.component1() + inputs.component2() + inputs.component3()
+                if (current > previous) count++
+                previous = current
+            }
+            inputs.removeFirst()
+        } catch (e: IndexOutOfBoundsException) {
+            break
+        }
+    }
     println(count)
 }
