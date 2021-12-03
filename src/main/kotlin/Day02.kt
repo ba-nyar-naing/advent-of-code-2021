2b11a5c988cadae9c227bcd9e141066a6886cdc0
Ba Nyar Naing <banyarnaing123@gmail.com>
2021-12-03 20:14:16 +0630

    solve day 2

---
+++ src/main/kotlin/Day02.kt
@@ -0,0 +1,46 @@
+fun main() {
+    /*  part 1
+    var horizontal = 0
+    var depth = 0
+
+    readLines("input02.txt").map { input ->
+        Pair(
+            input.split(" ")[0],
+            input.split(" ")[1].toInt()
+        )
+    }.forEach { course ->
+        when (course.first) {
+            "forward" -> horizontal += course.second
+            "up" -> depth -= course.second
+            "down" -> depth += course.second
+        }
+    }
+    println(horizontal * depth)
+     */
+
+    //  part 2
+    var horizontal = 0
+    var depth = 0
+    var aim = 0
+
+    readLines("input02.txt").map { input ->
+        Pair(
+            input.split(" ")[0],
+            input.split(" ")[1].toInt()
+        )
+    }.forEach { course ->
+        when (course.first) {
+            "forward" -> {
+                horizontal += course.second
+                depth += course.second * aim
+            }
+            "up" -> {
+                aim -= course.second
+            }
+            "down" -> {
+                aim += course.second
+            }
+        }
+    }
+    println(horizontal * depth)
+}
